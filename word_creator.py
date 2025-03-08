3c3d46d5301b4f682e93ac8d532a50ad9408d21b
Nevig Chanuka <nevig.chanuka@gmail.com>
2025-03-08 23:56:54 +0530

    update9

--- word_creator.py
+++ word_creator.py
@@ -289,7 +289,24 @@ def word_creator():
                 print(last_row_index)
 
 
-                # print(df_combined.iloc[1])
+def new_word_adder():
+    while True:
+        word = input("Word: ")
+
+        if word == '0':
+            break
+        else:
+            df = pd.read_parquet('vocab_data.parquet', engine='pyarrow')
+            result = df[df["words"].astype(str).str.contains(word, case=False)]
+            print(result)
+
+        add_word = input("Add: ")
+
+        if add_word == '':
+            with open('text.txt', 'a', encoding="utf-8") as file:
+                file.write(f'  {word}  ')
+
+        print("---------------------------------------------------")
 
 # def verb_finder():
 #     with open("text.txt", "r", encoding="utf-8") as text_file:
